7a2d3ef4894fda55d478493a235f9914e2d9e06c
Abdulrahman Fekry <abdofekryasda@gmail.com>
2025-05-29 03:23:56 +0300

    // refactor doctor card

--- lib/core/shared_widgets/card.dart
+++ lib/core/shared_widgets/card.dart
@@ -1,13 +1,13 @@
-import 'dart:math';
-
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:docway/features/doctor_details/logic/cubit/doctor_details_cubit.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import '../../features/doctor_details/logic/cubit/doctor_details_cubit.dart';
 import '../../features/doctor_details/presentation/doctor_details_ui.dart';
 import '../di/debendency_injection.dart';
+
 import '../theme/text_themes/text.dart';
 
 class CardDetails extends StatelessWidget {
@@ -19,9 +19,11 @@ class CardDetails extends StatelessWidget {
     required this.degree,
     required this.id,
     this.elevation,
-
   });
-  List<String> imagesDoctors=[
+
+  static int _currentImageIndex = 0;
+
+  final List<String> imagesDoctors = [
     "assets/doctors/covid-coronavirus-outbreak-healthcare-workers-pandemic-concept-seriouslooking-professional-male.jpg",
     "assets/doctors/portrait-smiling-male-doctor-dressed-uniform.jpg",
     "assets/doctors/thoughtful-young-male-doctor-wearing-medical-robe-stethoscope-around-his-neck-putting-hand-chin-isolated-white-wall.jpg",
@@ -31,32 +33,33 @@ class CardDetails extends StatelessWidget {
     "assets/doctors/young-man-doctor-wearing-white-coat-stethoscope-standing-with-skeptic-facial-expression-touching-his-chin-thinking.jpg",
   ];
 
-  String? imageDoctor;
-  String? name;
-  String? specialize;
-  String? degree;
-  int? id;
-  double? elevation = .1;
+  final String? imageDoctor;
+  final String? name;
+  final String? specialize;
+  final String? degree;
+  final int? id;
+  final double? elevation;
 
   @override
   Widget build(BuildContext context) {
+    int imageIndex = _currentImageIndex;
+    _currentImageIndex = (_currentImageIndex + 1) % imagesDoctors.length;
+
     return GestureDetector(
       onTap: () {
         Navigator.push(
           context,
           MaterialPageRoute(
-            builder:
-                (context) => BlocProvider(
-                  create:
-                      (BuildContext context) =>
-                          getIt<DoctorDetailsCubit>()..getDoctorDetails(id!),
-                  child: DoctorDetailsUi(),
-                ),
+            builder: (context) => BlocProvider(
+              create: (BuildContext context) =>
+              getIt<DoctorDetailsCubit>()..getDoctorDetails(id!),
+              child: DoctorDetailsUi(),
+            ),
           ),
         );
       },
       child: Card(
-        elevation: elevation,
+        elevation: elevation ?? .1,
         borderOnForeground: true,
         color: Colors.white,
         child: Padding(
@@ -65,9 +68,11 @@ class CardDetails extends StatelessWidget {
             children: [
               ClipRRect(
                 borderRadius: BorderRadius.all(Radius.circular(30.r)),
-                child: (imageDoctor == null || imageDoctor!.isEmpty || imageDoctor!.contains("via.placeholder.com"))
+                child: (imageDoctor == null ||
+                    imageDoctor!.isEmpty ||
+                    imageDoctor!.contains("via.placeholder.com"))
                     ? Image.asset(
-                  imagesDoctors[Random().nextInt(imagesDoctors.length)],
+                  imagesDoctors[imageIndex],
                   height: 110.h,
                   width: 110.w,
                   fit: BoxFit.cover,
@@ -76,10 +81,11 @@ class CardDetails extends StatelessWidget {
                   height: 110.h,
                   width: 110.w,
                   imageUrl: imageDoctor!,
-                  placeholder: (context, url) => const CircularProgressIndicator(),
+                  placeholder: (context, url) =>
+                  const CircularProgressIndicator(),
                   errorWidget: (context, url, error) {
                     return Image.asset(
-                      imagesDoctors[Random().nextInt(imagesDoctors.length)],
+                      imagesDoctors[imageIndex],
                       height: 110.h,
                       width: 110.w,
                       fit: BoxFit.cover,
